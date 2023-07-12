bb30ea350e3127a10dabb92ec9438d4d83ebff1e
Ribass98 <joserivs1998@gmail.com>
2023-07-11 22:18:44 -0600

    Se agrega el Proyecto

---
+++ app/Views/vw-footer.php
@@ -0,0 +1,13 @@
+<footer>
+  
+</footer>
+</div>
+</div>
+<script src="js/jquery-3.5.1.min.js" crossorigin="anonymous" ></script>
+<script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="js/simple-datatables.min.js" crossorigin="anonymous"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
